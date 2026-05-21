1970df0f50761b77aee9221c9f7051d6f49cd49f
gue1971 <gue1971@gmail.com>
2026-05-21 21:53:56 +0900

    Add economics and game theory phrase entries

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "kotoba-karute-v20260521-827";
+const CACHE_NAME = "kotoba-karute-v20260521-847";
 
 const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css?v=20260517-detail-footer",
-  "./phrases.js?v=20260521-827",
-  "./app.js?v=20260521-827",
+  "./phrases.js?v=20260521-847",
+  "./app.js?v=20260521-847",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
